b258ad9f97b3efde36731df7766ce9967f2d9926
741051180@qq.com <741051180@qq.com>
2018-12-06 10:29:12 +0800

    calculate

--- SSKJ.RoadManageSystem.API/Controllers/UserInfoController.cs
+++ SSKJ.RoadManageSystem.API/Controllers/UserInfoController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
